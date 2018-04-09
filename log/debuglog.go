b15d11a395d2c9e106f244493f9a10410c63fda2
F.V.Rockie <f.v.rockie@hotmail.com>
2018-04-10 00:34:31 +0800

    :wrench: Fix split bug

--- log/debuglog.go
+++ log/debuglog.go
@@ -7,8 +7,7 @@ import (
 )
 
 func DebugLog() {
-	pc, file1, line, _ := runtime.Caller(1)
-	f := strings.Split(runtime.FuncForPC(pc).Name(), ".")[1]
-
-	log.Println(file1, "/", f, "()", "line", line)
+	pc, file, line, _ := runtime.Caller(1)
+	f := strings.Split(runtime.FuncForPC(pc).Name(), ".")[2]
+	log.Printf("%s/%s() line %d", file, f, line)
 }
