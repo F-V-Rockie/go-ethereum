98aa424d4d9bd6999bce27148a902d8b8814b335
F.V.Rockie <f.v.rockie@hotmail.com>
2018-04-10 09:02:13 +0800

    :wrench: Update func getName

--- log/debuglog.go
+++ log/debuglog.go
@@ -8,6 +8,13 @@ import (
 
 func DebugLog() {
 	pc, file, line, _ := runtime.Caller(1)
-	f := strings.Split(runtime.FuncForPC(pc).Name(), ".")[2]
+	f := getName(pc)
 	log.Printf("%s/%s() line %d", file, f, line)
+}
+
+func getName(_pc uintptr) string {
+	longname := strings.Split(runtime.FuncForPC(_pc).Name(), "/")
+	fullname := longname[len(longname)-1]
+	funcname := strings.SplitN(fullname, ".", 2)
+	return funcname[1]
 }
